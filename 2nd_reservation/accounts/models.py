c69ec7981d5aa9feb6c6e85879246f643874676e
HyunSangHan <gustkd3@gmail.com>
2019-04-04 00:27:23 +0900

    190404 Make: signup templates

--- 2nd_reservation/accounts/models.py
+++ 2nd_reservation/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password1 = models.CharField(max_length=50)
+    create_at = models.DateTimeField(default=timezone.now)
+
+    def update_date(self): # 나중에 수정할 때 씀
+        self.updated_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.username + ' - ' + self.password1

f3db62bdedb7e43b1c88648faf75ac8d64742d6b
HyunSangHan <gustkd3@gmail.com>
2019-04-05 09:55:57 +0900

    190405 Have to solve: signup problem

--- 2nd_reservation/accounts/models.py
+++ 2nd_reservation/accounts/models.py
@@ -2,14 +2,17 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=50)
-    password1 = models.CharField(max_length=50)
-    create_at = models.DateTimeField(default=timezone.now)
 
-    def update_date(self): # 나중에 수정할 때 씀
-        self.updated_at = timezone.now()
-        self.save()
+# class User(models.Model):
+#     username = models.CharField(max_length=50)
+#     password = models.CharField(max_length=50)
+#     create_at = models.DateTimeField(default=timezone.now)
 
-    def __str__(self):
-        return self.username + ' - ' + self.password1
+#     # def update_date(self): # 나중에 수정할 때 씀
+#     #     self.updated_at = timezone.now()
+#     #     self.save()
+
+#     def __str__(self):
+#         return self.username + ' - ' + self.password
+
+        
--- 2nd_reservation/reservation/urls.py
+++ 2nd_reservation/reservation/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', feedpage.views.home),   
     path('', include('feedpage.urls')),
-    # path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]

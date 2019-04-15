d92289b206cc93d3dbbcd708998fb508889a5ea4
HyunSangHan <gustkd3@gmail.com>
2019-04-16 01:02:40 +0900

    190416 Make: more seed for classification

--- 2nd_ugc/feeds/models.py
+++ 2nd_ugc/feeds/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from faker import Faker
 # from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 import random
 # Create your models here.
 
@@ -21,9 +22,26 @@ class Feed(models.Model):
     def seed(count):
             myfake = Faker('ko_KR')
             for i in range(count):
+                rand = random.randrange(1,9)
+                if rand == 1:
+                    tmp = '연예'
+                elif rand == 2:
+                    tmp = '스포츠'
+                elif rand == 3:
+                    tmp = '정치'
+                elif rand == 4:
+                    tmp = '경제'
+                elif rand == 5:
+                    tmp = '사회'
+                elif rand == 6:
+                    tmp = '생활/문화'
+                elif rand == 7:
+                    tmp = '세계'
+                else:
+                    tmp = 'IT/과학'
                 Feed.objects.create(
                     title=myfake.bs(),
-                    category = '생활/문화',
+                    category = tmp,
                     writer=myfake.name(),
                     content=myfake.catch_phrase() + ' 속에 ' + myfake.catch_phrase() + ' 제시',
                 )

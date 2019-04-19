5b92bebe80727d200a2f8255b5603ebf9d736a53
HyunSangHan <gustkd3@gmail.com>
2019-04-19 14:45:11 +0900

    190419 Add: listing box css

--- 2nd_ugc/ugc/settings.py
+++ 2nd_ugc/ugc/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
     'feeds.apps.FeedsConfig',
+    # 'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -119,5 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+) 
